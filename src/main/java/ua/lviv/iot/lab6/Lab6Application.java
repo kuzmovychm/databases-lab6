756985b4e54b3f6ab5019696472aaef267faf0a7
Max Kuzmovych <kuzmovychm@gmail.com>
2020-10-01 22:40:55 +0300

    extend Application from SpringBootServletInitializer, exclude embeded tomcat

--- src/main/java/ua/lviv/iot/lab6/Lab6Application.java
+++ src/main/java/ua/lviv/iot/lab6/Lab6Application.java
@@ -2,9 +2,16 @@ package ua.lviv.iot.lab6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Lab6Application {
+public class Lab6Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(Lab6Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab6Application.class, args);
